0ba402ed5f47f18d1f40cc9c780b4a9bb88a2c54
ARTUSI <xavier.artusi@cea.fr>
2021-08-31 11:30:59 +0200

    remove main from package.json to avoid bokeh to load a non existent cdn

--- examples/example.py
+++ examples/example.py
@@ -1,11 +1,8 @@
 import panel as pn
 from bokeh.palettes import Viridis256
 from bokeh.sampledata.autompg import autompg_clean as df
-from pcp import parallel_plot
+from pcp import ParallelCoordinates
 
 
 # make bokeh parallel plot
-p = parallel_plot(df, drop="name", color=df.origin, palette=Viridis256)
-p.xaxis.axis_label = ' '
-p.title = "Parallel Coordinates"
-pn.panel(p, sizing_mode="stretch_both", height=500).show()
+ParallelCoordinates(df, drop=["name"]).show()
